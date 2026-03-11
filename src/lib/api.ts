091a0f09dc66235457d15868785409eec504edbd
Sahed <sahed@Saheds-Mac-mini.local>
2026-03-11 16:19:54 +0600

    add: homepage main articles ppartial

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ArticleQueryParams } from "./types";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -13,6 +14,19 @@ export async function getAllcategories() {
     return response.data;
   } catch (error) {
     console.error("Error fetching categories:", error);
+    throw error;
+  }
+}
+
+// Get articles
+export async function getArticles(query: ArticleQueryParams) {
+  try {
+    const response = await api.get("/web/articles", {
+      params: query,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error);
     throw error;
   }
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,3 +11,55 @@ export interface Category {
   position: number | null;
   subCategories: Category[];
 }
+
+export interface ArticleQueryParams {
+  page?: number;
+  limit?: number;
+  searchTerm?: string;
+  isActive?: boolean;
+  isExclusive?: boolean;
+  isFeatured?: boolean;
+  categoryId?: string; // category uuid
+  subCategoryId?: string; // category uuid
+  authorId?: string; // author uuid
+  type?: "news" | "poll" | "series" | "stories";
+  status?: "Drafted" | "Published" | "Archived";
+}
+
+export interface Author {
+  id: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  nameBn: string;
+  image: string;
+  designation: string;
+  article: number;
+  designationBn: string;
+}
+
+export interface Article {
+  id: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+  title: string;
+  code: string;
+  slug: string;
+  type: string;
+  position: number | null;
+  coverImage: string;
+  excerpt: string;
+  details: string;
+  language: "English" | "Bengali" | string;
+  status: "Published" | "Drafted" | "Archived";
+  date: string;
+  isExclusive: boolean;
+  isFeatured: boolean;
+  authorId: string;
+  categoryId: string;
+  tags: string[];
+  author: Author;
+  category: Category;
+}
